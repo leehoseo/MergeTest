56c378da9db63269ff93f9f5dbcbe09a66fa685d
Hongkyu <ys37003@gmail.com>
2017-03-08 21:03:49 +0900

    test

---
+++ Assets/test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("!23");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
